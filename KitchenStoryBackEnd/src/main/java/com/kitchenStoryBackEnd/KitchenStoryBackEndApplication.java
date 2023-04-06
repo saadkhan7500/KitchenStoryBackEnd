35e2d8c544d99a1413e78b0c545f9b85520b6808
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-04-07 00:16:55 +0530

    first commit

---
+++ KitchenStoryBackEnd/src/main/java/com/kitchenStoryBackEnd/KitchenStoryBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.kitchenStoryBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenStoryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenStoryBackEndApplication.class, args);
+	}
+
+}
---
+++ KitchenStoryBackEnd/src/test/java/com/kitchenStoryBackEnd/KitchenStoryBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenStoryBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStoryBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
