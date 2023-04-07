b70860479a6d72e47302895e088d0c22a0efe5ec
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-04-08 00:51:02 +0530

    added user authentication

--- KitchenStoryBackEnd/src/main/java/com/kitchenStoryBackEnd/Controller/UserController.java
+++ KitchenStoryBackEnd/src/main/java/com/kitchenStoryBackEnd/Controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kitchenStoryBackEnd.Authentication.UserAuthentication;
 import com.kitchenStoryBackEnd.Entities.User;
 import com.kitchenStoryBackEnd.Repo.UserRepo;
 
@@ -45,7 +46,7 @@ public class UserController {
 	
 	//API to authenticate the user at the time of login
 	@PostMapping("/checkUser")
-	User  AuthenticateUser(@RequestBody User user)
+	User  AuthenticateUser(@RequestBody UserAuthentication user)
 	{
 		System.out.println("Authenticate user API calling");
 		User u = repo.findByEmail(user.getEmail());
