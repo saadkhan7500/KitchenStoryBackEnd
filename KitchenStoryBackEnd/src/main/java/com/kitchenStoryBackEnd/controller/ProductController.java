b43cee26f678569c4bc9d2a1af6a56ee4f389714
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-04-13 17:47:11 +0530

    uploaded image

--- KitchenStoryBackEnd/src/main/java/com/kitchenStoryBackEnd/controller/ProductController.java
+++ KitchenStoryBackEnd/src/main/java/com/kitchenStoryBackEnd/controller/ProductController.java
@@ -1,32 +1,59 @@
 package com.kitchenStoryBackEnd.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kitchenStoryBackEnd.entities.Product;
+import com.kitchenStoryBackEnd.repo.ProductRepo;
 import com.kitchenStoryBackEnd.service.ProductService;
 
 @RestController
+@CrossOrigin
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
+	@Autowired
+	private ProductRepo productRepo;
+
 	@PostMapping("/addProduct")
-	public Product addProduct(@RequestBody Product product)
-	{
+	public Product addProduct(@RequestBody Product product) {
 		return productService.addProduct(product);
 	}
-	
+
 	@GetMapping("/getProducts")
-	public List<Product> getProducts()
-	{
+	public List<Product> getProducts() {
 		return productService.getProducts();
 	}
+
 	
+	@PutMapping("/uploadImage/{id}")
+	public void uploadImage(@PathVariable("id") int id,@RequestBody String url)
+	{
+		System.out.println("calling uplaodImage api.....");
+		System.out.println(id);
+		System.out.println(url);
+		
+		Product product=productRepo.findById(id);
+		product.setImgname(url);
+		productRepo.save(product);
+	}
+
 }
